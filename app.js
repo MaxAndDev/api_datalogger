8bc1e3b5da29516249b09f417ebf5a1a65843936
Max <nessenmaxi@googlemail.com>
2019-10-23 14:15:39 +0200

    handling CORS

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use('/data', dataRoutes);
 app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
+    res.header('Access-Controll-Allow-Origin', '*');
+    res.header('Access-Controll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Controll-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({});
+    }
+});
+
+app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
     next(error);
