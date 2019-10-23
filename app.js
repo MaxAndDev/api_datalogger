627befef55634d983d5aef13ea69bbe781eb4e67
Max <nessenmaxi@googlemail.com>
2019-10-23 11:50:01 +0200

    add default error behavior

--- app.js
+++ app.js
@@ -12,4 +12,17 @@ app.use(morgan('combined', { stream: logger.stream }));
 app.use('/data', dataRoutes);
 app.use('/user', userRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: error.message
+    });
+});
+
 module.exports = app;
