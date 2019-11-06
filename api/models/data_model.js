20eab125698fc678fd92b78016b2566f5501a056
Max <nessenmaxi@googlemail.com>
2019-11-06 11:47:30 +0100

    change model

--- api/models/data_model.js
+++ api/models/data_model.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const dataSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    station_id: { type: Number, required: true},
-    airpressure: { type: Number, required: true},
-    humidity: { type: Number, required: true},
-    temperature: { type: Number, required: true},
-    timestamp: { type: Number, required: true},
-    latitude: { type: Number, required: true},
-    longitude: { type: Number, required: true},
-    position_tag: { type: String, required: true},
+    station_id: { type: Number},
+    airpressure: { type: Number},
+    humidity: { type: Number},
+    temperature: { type: Number},
+    timestamp: { type: Number},
+    latitude: { type: Number},
+    longitude: { type: Number},
+    position_tag: { type: String},
 });
 
 module.exports = mongoose.model('Data', dataSchema);
