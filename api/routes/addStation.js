0ace77cfae00271c2905747c9d6f231fda074405
Max <nessenmaxi@googlemail.com>
2020-01-03 22:38:19 +0100

    without aes

--- api/routes/addStation.js
+++ api/routes/addStation.js
@@ -21,7 +21,7 @@ router.post('/add', (req, res, next) => {
         position_tag: req.body.position_tag,
     });
 
-    station.save()
+    station.save()  
     .then( result => {
         const message_object = {
             station_id: result._id,
@@ -30,17 +30,21 @@ router.post('/add', (req, res, next) => {
         };
         logger.info(message_object);
 
-        let iv = crypto.randomBytes(16);
+        /* let iv = crypto.randomBytes(16);
+
+        // iv length in base64 is 16 but if you encode it to hex it is 32
+
         let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(process.env.PRIVATE), iv);
         let encrypted = cipher.update(secret.toString('hex'));
 
-        encrypted = Buffer.concat([encrypted, cipher.final()]);
+        encrypted = Buffer.concat([encrypted, cipher.final()]); */
 
         res.status(200).json({
             station_id: result._id,
             api_key: key.apiKey,
-            iv: iv.toString('hex'),
-            encrypted: encrypted.toString('hex')
+            secret: secret.toString('hex')
+            //iv: iv.toString('hex'),
+            //encrypted: encrypted.toString('hex')
         })
 
     }).catch( err => {
