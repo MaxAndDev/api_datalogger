60849604ca4ab100ec24ec3f9939b47d60d64962
Max <nessenmaxi@googlemail.com>
2020-01-05 17:23:50 +0100

    decryption for base64 encoded stuff

--- api/models/data_model.js
+++ api/models/data_model.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const dataSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    station_id: { type: Number},
+    station_id: { type: String },
     airpressure: { type: Number},
     humidity: { type: Number},
     temperature: { type: Number},
-    timestamp: { type: Number},
-    latitude: { type: Number},
-    longitude: { type: Number},
-    position_tag: { type: String},
+    timestamp: { type: Number}
 });
 
 module.exports = mongoose.model('Data', dataSchema);
